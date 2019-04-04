7ef2e15ca72847c65f76feebc5f29b390dd58c9a
Nicholas Grogg <nicholas_grogg6505@hotmail.com>
2019-04-04 02:01:46 +0000

    Update CS421-CLI.py added the if/elif/else statements
    Still need:
    MySQL stuff

--- CS421-CLI.py
+++ CS421-CLI.py
@@ -4,8 +4,8 @@ import argparse
 import mysql.connector
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", action='store', dest='table',    help="Add to table A")
-parser.add_argument("-t", action='store', dest='textfile', help="Use text file T")
+parser.add_argument("-a", action='store', dest='table', required=True, help="Add to table A")
+parser.add_argument("-t", action='store', dest='textfile', required=True, help="Use text file T")
 args = parser.parse_args()
 
 # the name of the file
@@ -16,35 +16,137 @@ Table    = args.table
 #mysql.connector.connect(host='localhost',database='l',user='',password='')
 
 f = open(fileName)
-#f = open('sample.txt')
 line = f.readline()
 
 # If table flag is C for car table
+if Table == 'C':
+    while line:
+        line.split()
+        a,b,c,d,e,f,g=line.split()
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+        print(f)
+        print(g)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is B for brand table
-while line:
-    #print(line)
-    line.split()
-    a,b,c=line.split()
-    print(a)
-    print(b)
-    print(c)
-    # MySQL stuff goes here
-    line = f.readline()
-f.close()
+elif Table == 'B':
+    while line:
+        line.split()
+        a,b,c=line.split()
+        print(a)
+        print(b)
+        print(c)
+        # MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is E for Engine table
+elif Table == 'E':
+    while line:
+        line.split()
+        a,b,c,d,e,f,g=line.split()
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+        print(f)
+        print(g)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is BT for Body Type table
+elif Table == 'BT':
+    while line:
+        line.split()
+        a,b,c,d,e=line.split()
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is T for Tech table
+elif Table == 'T':
+    while line:
+        line.split()
+        a,b,c,d,e,f=line.split()
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+        print(f)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is R for Review table
+elif Table == 'R':
+    while line:
+        line.split()
+        a,b,c=line.split()
+        print(a)
+        print(b)
+        print(c)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is CO for Cost of Ownership table
+elif Table == 'CO':
+    while line:
+        line.split()
+        a,b,c,d,e=line.split()
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is UE for Uses Engine table
+elif Table == 'UE':
+    while line:
+        line.split()
+        a,b=line.split()
+        print(a)
+        print(b)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
 # If table flag is JP for Joint Project table
+elif Table == 'JP':
+    while line:
+        line.split()
+        a,b,c,d,e,f,g,h,i=line.split()
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+        print(f)
+        print(g)
+        print(h)
+        print(i)
+        #MySQL stuff goes here
+        line = f.readline()
+    f.close()
 
+# Fail state for invalid options
+else:
+    print "Invalid parameter"
 # Close MySQL connection
