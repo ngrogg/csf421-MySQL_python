0ab37cd80cfa85bc0960a1a9e7d4cd16c0680305
Nicholas Grogg <nicholas_grogg6505@hotmail.com>
2019-04-27 22:42:12 +0000

    Update CS421-GUI.py
    Created new forms for each command, can now go forwards and backwards

--- CS421-GUI.py
+++ CS421-GUI.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python2.7
 
 import mysql.connector
-#import Tkinter
 import tkMessageBox
-
 from Tkinter import *
+# Fuck it, import everything!
 
 class Application(Frame):
 
+    # When go back button is hit on query screen
+    def back_query(self):
+        self.button1.pack_forget()
+        self.createWidgets()
+
+    # When go back button is hit on insertion screen
+    def back_insertion(self):
+        self.buttona.pack_forget()
+        self.createWidgets()
+
     # Button to run query
     def query(self):
-        print "test for now"
-        
+        # Insert title text
+        self.Title = 
+        # Remove other buttons
+
+        self.INSERTION.pack_forget()
+        self.QUIT.pack_forget()
+        self.QUERY.pack_forget()
+
+        # create Go Back button
+        self.button1 = Button(self)
+        self.button1["text"] = "Back"
+        self.button1["command"] = self.back_query
+        self.button1.pack({"side" : "left"})
+
+
     # Button to insert data
     def insertion(self):
-        print "test for now"
+        self.INSERTION.pack_forget()
+        self.QUIT.pack_forget()
+        self.QUERY.pack_forget()
+
+        # Create Go Back button
+        self.buttona = Button(self)
+        self.buttona["text"] = "Back"
+        self.buttona["command"] = self.back_insertion
+        self.buttona.pack({"side" : "left"})
 
     def createWidgets(self):
         # Button Parameters
