1c31e1ee84b9ad1d6f45d57d1e6351e8aca4edd3
Nicholas Grogg <nicholas_grogg6505@hotmail.com>
2019-04-28 00:16:42 +0000

    Update CS421-GUI.py
    Created all the pages for Query/Insertion/ExportTable/ExportQuery/Removal

--- CS421-GUI.py
+++ CS421-GUI.py
@@ -9,43 +9,137 @@ class Application(Frame):
 
     # When go back button is hit on query screen
     def back_query(self):
-        self.button1.pack_forget()
+        # Remove created buttons
+        self.buttona.pack_forget()
+
+        # Create Home buttons
         self.createWidgets()
 
     # When go back button is hit on insertion screen
     def back_insertion(self):
-        self.buttona.pack_forget()
+        # Remove created buttons
+        self.buttonb.pack_forget()
+       
+        # Create Home buttons
+        self.createWidgets()
+
+    # When go back button is hit on removal screen
+    def back_removal(self):
+        # Remove created buttons
+        self.buttonc.pack_forget()
+
+        # Create Home buttons
+        self.createWidgets()
+
+    # When go back button is hit on export table screen
+    def back_exportTable(self):
+        # Remove created buttons
+        self.buttond.pack_forget()
+
+        # Create Home buttons
+        self.createWidgets()
+
+    # When go back button is hit on export query screen
+    def back_exportQuery(self):
+        # Remove created buttons
+        self.buttone.pack_forget()
+
+        # Create Home buttons
         self.createWidgets()
 
     # Button to run query
     def query(self):
-        # Insert title text
-        self.Title = 
-        # Remove other buttons
+       # Insert title text
 
+        # Remove other buttons
         self.INSERTION.pack_forget()
         self.QUIT.pack_forget()
         self.QUERY.pack_forget()
+        self.REMOVAL.pack_forget()
+        self.EXPORTQUERY.pack_forget()
+        self.EXPORTTABLE.pack_forget()
 
         # create Go Back button
-        self.button1 = Button(self)
-        self.button1["text"] = "Back"
-        self.button1["command"] = self.back_query
-        self.button1.pack({"side" : "left"})
+        self.buttona = Button(self)
+        self.buttona["text"] = "Back"
+        self.buttona["command"] = self.back_query
+        self.buttona.pack({"side" : "left"})
 
 
     # Button to insert data
     def insertion(self):
+        # Insert title text
+
+        # Remove other buttons
         self.INSERTION.pack_forget()
         self.QUIT.pack_forget()
         self.QUERY.pack_forget()
+        self.REMOVAL.pack_forget()
+        self.EXPORTQUERY.pack_forget()
+        self.EXPORTTABLE.pack_forget()
 
         # Create Go Back button
-        self.buttona = Button(self)
-        self.buttona["text"] = "Back"
-        self.buttona["command"] = self.back_insertion
-        self.buttona.pack({"side" : "left"})
+        self.buttonb = Button(self)
+        self.buttonb["text"] = "Back"
+        self.buttonb["command"] = self.back_insertion
+        self.buttonb.pack({"side" : "left"})
 
+    # Button to remove data
+    def removal(self):
+        # Insert title text
+
+        # Remove Home buttons
+        self.INSERTION.pack_forget()
+        self.QUIT.pack_forget()
+        self.QUERY.pack_forget()
+        self.REMOVAL.pack_forget()
+        self.EXPORTQUERY.pack_forget()
+        self.EXPORTTABLE.pack_forget()
+
+        # Create Go Back button
+        self.buttonc = Button(self)
+        self.buttonc["text"] = "Back"
+        self.buttonc["command"] = self.back_removal
+        self.buttonc.pack({"side" : "left"})
+
+
+    # Button to export table
+    def exportTable(self):
+        # Insert title text
+
+        # Remove Home buttons
+        self.INSERTION.pack_forget()
+        self.QUIT.pack_forget()
+        self.QUERY.pack_forget()
+        self.REMOVAL.pack_forget()
+        self.EXPORTQUERY.pack_forget()
+        self.EXPORTTABLE.pack_forget()
+
+        # Create Go Back button
+        self.buttond = Button(self)
+        self.buttond["text"] = "Back"
+        self.buttond["command"] = self.back_exportTable
+        self.buttond.pack({"side" : "left"})
+
+    # Button to export query
+    def exportQuery(self):
+        # Insert title text
+
+        # Remove Home buttons
+        self.INSERTION.pack_forget()
+        self.QUIT.pack_forget()
+        self.QUERY.pack_forget()
+        self.REMOVAL.pack_forget()
+        self.EXPORTQUERY.pack_forget()
+        self.EXPORTTABLE.pack_forget()
+
+        # Create Go Back button
+        self.buttone = Button(self)
+        self.buttone["text"] = "Back"
+        self.buttone["command"] = self.back_exportQuery
+        self.buttone.pack({"side" : "left"})
+
+    # Home page 
     def createWidgets(self):
         # Button Parameters
         self.QUIT = Button(self)
@@ -58,7 +152,7 @@ class Application(Frame):
 
         # Button parameters for Query button
         self.QUERY = Button(self)
-        self.QUERY["text"] = "Query",
+        self.QUERY["text"] = "Query"
         self.QUERY["command"] = self.query
 
         # Add Query Button to GUI
@@ -66,12 +160,29 @@ class Application(Frame):
 
         # Button Parameters for Insertion button
         self.INSERTION = Button(self)
-        self.INSERTION["text"] = "Insertion",
+        self.INSERTION["text"] = "Insertion"
         self.INSERTION["command"] = self.insertion
 
         # Create Button
         self.INSERTION.pack({"side": "left"})
 
+        # Button for Removal
+        self.REMOVAL = Button(self)
+        self.REMOVAL["text"] = "Removal"
+        self.REMOVAL["command"] = self.removal
+        self.REMOVAL.pack({"side": "left"})
+
+        # Button for Exporting table
+        self.EXPORTTABLE = Button(self)
+        self.EXPORTTABLE["text"] = "ExportTable"
+        self.EXPORTTABLE["command"] = self.exportTable
+        self.EXPORTTABLE.pack({"side": "left"})
+
+        # Button for Exporting Query
+        self.EXPORTQUERY = Button(self)
+        self.EXPORTQUERY["text"] = "ExportQuery"
+        self.EXPORTQUERY["command"] = self.exportQuery
+        self.EXPORTQUERY.pack({"side":"left"})
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
