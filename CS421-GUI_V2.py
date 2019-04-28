2fb33ac1dea9af83a40fd7b18f8ca4d3a300a480
Nicholas Grogg <nicholas_grogg6505@hotmail.com>
2019-04-28 22:36:59 +0000

    Update CS421-GUI_V2.py
    Added to the insertion section. There are now text boxes that assign to variables. All that's left to do for that section is to assign the variable to a query. The code for that is in the CLI.py script

--- CS421-GUI_V2.py
+++ CS421-GUI_V2.py
@@ -270,6 +270,53 @@ class QueryPage(tk.Frame):
 #Insert Page#
 #############
 class InsertPage(tk.Frame):
+        def INSERTION(self):
+            # Assign text box stuff to variable
+            self.make = self.make_entry.get() 
+            self.model = self.model_entry.get()
+            self.price = self.price_entry.get()
+            self.platform = self.platform_entry.get()
+            self.drive = self.drive_entry.get()
+            self.aggrev = self.aggrev_entry.get()
+            self.worth = self.worth_entry.get()
+            self.public = self.public_entry.get()
+            self.critic = self.critic_entry.get()
+            self.customer = self.customer_entry.get()
+            self.construction = self.construction_entry.get()
+            self.shape = self.shape_entry.get()
+            self.suspension = self.suspension_entry.get()
+            self.infotainment = self.infotainment_entry.get()
+            self.proximity = self.proximity_entry.get()
+            self.autobrake = self.autobrake_entry.get()
+            self.adc = self.adc_entry.get()
+            self.maintenance = self.main_entry.get()
+            self.dep = self.dep_entry.get()
+            self.recall = self.recalls_entry.get()
+
+            # Output variables for testing
+            print self.make 
+            print self.model
+            print self.price 
+            print self.platform 
+            print self.drive
+            print self.aggrev 
+            print self.worth
+            print self.public 
+            print self.critic 
+            print self.customer 
+            print self.construction 
+            print self.shape 
+            print self.suspension 
+            print self.infotainment 
+            print self.proximity
+            print self.autobrake 
+            print self.adc
+            print self.maintenance 
+            print self.dep 
+            print self.recall
+
+            # Assign vairables to mysql statements and execute
+
 	def __init__(self,parent,controller):
 		tk.Frame.__init__(self,parent)
 		_bgcolor = '#d9d9d9'  # X11 color: 'gray85'
@@ -282,103 +329,148 @@ class InsertPage(tk.Frame):
 		label = tk.Label(self,text="Please fill out the form to insert a car into the database. No box may be left empty.")
 		label.pack()
 
+        ### Fields for Query ###
+
+        ## Car Table ##
+        # Make
+        self.make_label = tk.Label(self, text = "Make: ")
+        self.make_label.pack()
+        self.make_entry = tk.Entry(self)
+        self.make_entry.pack()
+                
+        # Model
+        self.model_label = tk.Label(self, text = "Model: ")
+        self.model_label.pack()
+        self.model_entry = tk.Entry(self)
+        self.model_entry.pack()
+        
+        # Price
+        self.price_label = tk.Label(self, text = "Price: ")
+        self.price_label.pack()
+        self.price_entry = tk.Entry(self)
+        self.price_entry.pack()
+
+        # Platform Code
+        self.platform_label = tk.Label(self, text = "Platform Code: ")
+        self.platform_label.pack()
+        self.platform_entry = tk.Entry(self)
+        self.platform_entry.pack()
+
+                # Drive
+        self.drive_label = tk.Label(self, text = "Drive:")
+        self.drive_label.pack()
+        self.drive_entry = tk.Entry(self)
+        self.drive_entry.pack()
+
+        # Aggregate Review %
+        self.aggrev_label = tk.Label(self, text = "Aggregate Review: (00.00)")
+        self.aggrev_label.pack()
+        self.aggrev_entry= tk.Entry(self)
+        self.aggrev_entry.pack()
+
+        # Autonomous
+        self.autonomous_label = tk.Label(self, text = "Autonomous? (1/0)")
+        self.autonomous_label.pack()
+        self.autonomous_entry = tk.Entry(self)
+        self.autonomous_entry.pack()
+
+        ## Brand Table ##
+        # Brand Worth
+        self.worth_label = tk.Label(self, text = "Worth: (00.00)")
+        self.worth_label.pack()
+        self.worth_entry = tk.Entry(self)
+        self.worth_entry.pack()
+                        
+        # Public
+        self.public_label = tk.Label(self, text = "Public? (1/0)")
+        self.public_label.pack()
+        self.public_entry = tk.Entry(self)
+        self.public_entry.pack()
+        
+        ## Review Table ##
+        # Critic Score
+        self.critic_label = tk.Label(self, text = "Critic Score")
+        self.critic_label.pack()
+        self.critic_entry = tk.Entry(self)
+        self.critic_entry.pack()
+
+        # Customer Score
+        self.customer_label = tk.Label(self, text = "Customer score")
+        self.customer_label.pack()
+        self.customer_entry = tk.Entry(self)
+        self.customer_entry.pack()
+
+        ## Body Type ##
+        # Construction
+        self.construction_label = tk.Label(self, text = "Construction")
+        self.construction_label.pack()
+        self.construction_entry = tk.Entry(self)
+        self.construction_entry.pack()
+
+        # Shape
+        self.shape_label = tk.Label(self, text = "Shape")
+        self.shape_label.pack()
+        self.shape_entry = tk.Entry(self)
+        self.shape_entry.pack()
+
+        # Suspension
+        self.suspension_label = tk.Label(self, text = "Suspension")
+        self.suspension_label.pack()
+        self.suspension_entry = tk.Entry(self)
+        self.suspension_entry.pack()
+
+        ## Tech ##
+        # Infotainment Name
+        self.infotainment_label = tk.Label(self, text = "Infotainment: (No spaces)")
+        self.infotainment_label.pack()
+        self.infotainment_entry = tk.Entry(self)
+        self.infotainment_entry.pack()
+
+        # Proximity Sensor
+        self.proximity_label = tk.Label(self, text = "Proximity Sensor? (1/0)")
+        self.proximity_label.pack()
+        self.proximity_entry = tk.Entry(self)
+        self.proximity_entry.pack()
+
+        # Automatic Braking
+        self.autobrake_label = tk.Label(self, text = "Automatic Braking? (1/0)")
+        self.autobrake_label.pack()
+        self.autobrake_entry = tk.Entry(self)
+        self.autobrake_entry.pack()
+
+        # Adaptive Cruise Control
+        self.adc_label = tk.Label(self, text = "Adaptive Cruise? (1/0)")
+        self.adc_label.pack()
+        self.adc_entry = tk.Entry(self)
+        self.adc_entry.pack()
+
+        ## Cost of Ownership
+        # Average Maintenance 
+        self.main_label = tk.Label(self, text = "Average Maintenance: ")
+        self.main_label.pack()
+        self.main_entry = tk.Entry(self)
+        self.main_entry.pack()
+
+        # Annual Depreciation
+        self.dep_lab = tk.Label(self, text = "Annual Depreciation: (00.00)")
+        self.dep_lab.pack()
+        self.dep_entry = tk.Entry(self)
+        self.dep_entry.pack()
+
+        # Recalls
+        self.recalls_label = tk.Label(self, text = "Recalls: " )
+        self.recalls_label.pack()
+        self.recalls_entry = tk.Entry(self)
+        self.recalls_entry.pack()
+
+        # Back button
 		backbutton = tk.Button(self, text="Back",command = lambda: controller.show_frame(StartPage))
 		backbutton.pack(anchor = "sw", side = "left")
 
-###################		
-#Export Query Page#
-###################
-class QEPage(tk.Frame):
-	def __init__(self,parent,controller):
-		tk.Frame.__init__(self,parent)
-		_bgcolor = '#d9d9d9'  # X11 color: 'gray85'
-		_fgcolor = '#000000'  # X11 color: 'black'
-		_compcolor = '#d9d9d9' # X11 color: 'gray85'
-		_ana1color = '#d9d9d9' # X11 color: 'gray85'
-		_ana2color = '#ececec' # Closest X11 color: 'gray92'
-	        self.style = ttk.Style()
-
-		label = tk.Label(self,text="Please fill out the form to query the database. No box may be left empty.")
-		label.pack()
-		backbutton = tk.Button(self, text="Back",command = lambda: controller.show_frame(StartPage))
-		backbutton.pack(anchor = "sw", side = "left")
-
-	#begin Query form code
-		
-        	self.Label1 = tk.Label(self)
-       		self.Label1.place(relx=0.183, rely=0.089, height=28, width=46)
-        	self.Label1.configure(activebackground="#f9f9f9")
-        	self.Label1.configure(text='''SELECT''')
-
-        	self.Label2 = tk.Label(self)
-        	self.Label2.place(relx=0.183, rely=0.2, height=18, width=46)
-        	self.Label2.configure(activebackground="#f9f9f9")
-        	self.Label2.configure(text='''FROM''')
-
-        	self.Label3 = tk.Label(self)
-        	self.Label3.place(relx=0.183, rely=0.311, height=18, width=46)
-        	self.Label3.configure(activebackground="#f9f9f9")
-        	self.Label3.configure(text='''WHERE''')
-		
-	# Select input box
-	        self.Entry1 = tk.Entry(self)
-	        self.Entry1.place(relx=0.317, rely=0.089,height=20, relwidth=0.243)
-	        self.Entry1.configure(background="white")
-	        self.Entry1.configure(font="TkFixedFont")
-	        self.Entry1.configure(selectbackground="#c4c4c4")
-	# FROM input box
-	        self.Entry2 = tk.Entry(self)
-	        self.Entry2.place(relx=0.317, rely=0.2,height=20, relwidth=0.243)
-	        self.Entry2.configure(background="white")
-	        self.Entry2.configure(font="TkFixedFont")
-	        self.Entry2.configure(selectbackground="#c4c4c4")
-	#WHERE STATEMENT
-	    # WHERE input box1
-	        self.Entry3 = tk.Entry(self)
-	        self.Entry3.place(relx=0.317, rely=0.311,height=20, relwidth=0.243)
-	        self.Entry3.configure(background="white")
-	        self.Entry3.configure(font="TkFixedFont")
-	        self.Entry3.configure(selectbackground="#c4c4c4")
-	    # WHERE Input box2
-	        self.Entry4 = tk.Entry(self)
-	        self.Entry4.place(relx=0.633, rely=0.311,height=20, relwidth=0.243)
-	        self.Entry4.configure(background="white")
-	        self.Entry4.configure(font="TkFixedFont")
-	    # Option checkbox for WHERE clause
-	        self.CheckVar2 = tk.IntVar()
-	        self.RadioWHERE = tk.Checkbutton(self, variable = self.CheckVar2, onvalue = 1, offvalue = 0)
-	        self.RadioWHERE.place(relx=0.017, rely=0.311, relheight=0.044
-                , relwidth=0.132)
-	        self.RadioWHERE.configure(activebackground="#f9f9f9")
-	        self.RadioWHERE.configure(justify='left')
-	        self.RadioWHERE.configure(text='''WHERE?''')
-	    # Drop down menu for WHERE statement logicals
-	        self.TCombobox1 = ttk.Combobox(self)
-	        self.TCombobox1.place(relx=0.567, rely=0.311, relheight=0.04
-                , relwidth=0.062)
-	        self.value_list = ['AND','OR','>','<','=','!=']
-	        self.TCombobox1.configure(values=self.value_list)
-	        self.TCombobox1.configure(takefocus="")
-	# Query OutPut box
-	        #self.Scrolledtext1 = tk.scrolledtext.ScrolledText(self)
-	        #self.Scrolledtext1.place(relx=0.0, rely=0.667, relheight=0.262
-                #, relwidth=0.98)
-	        #self.Scrolledtext1.configure(background="white")
-	        #self.Scrolledtext1.configure(font="TkTextFont")
-	        #self.Scrolledtext1.configure(insertborderwidth="3")
-	        #self.Scrolledtext1.configure(selectbackground="#c4c4c4")
-	        #self.Scrolledtext1.configure(width=10)
-	        #self.Scrolledtext1.configure(wrap="none")
-	# Export button
-	        self.ExportQuery = tk.Button(self)
-	        self.ExportQuery.place(relx=0.35, rely=0.933, height=28, width=149)
-	        self.ExportQuery.configure(activebackground="#f9f9f9")
-	        self.ExportQuery.configure(text='''Export''')
-	# Run Query button
-	        self.Button1 = tk.Button(self)
-	        self.Button1.place(relx=0.017, rely=0.578, height=28, width=92)
-	        self.Button1.configure(text='''Run Query''')
-		
+        # Submit button
+        submit = tk.Button(self, text='Insert',command=self.INSERTION)
+        submit.pack(anchor = "sw", side = "right")
+	
 
 #############		
 #Remove Page#
