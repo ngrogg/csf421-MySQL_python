a76dd8042966f0cd2dadd1e1b894f2804fe834a5
Nicholas Grogg <nicholas_grogg6505@hotmail.com>
2019-12-03 18:55:00 +0000

    Changed formatting and added a comment block explaining what to change if re-purposing the script

--- CS421-CLI.py
+++ CS421-CLI.py
@@ -3,12 +3,20 @@
 import argparse
 import mysql.connector
 
+'''
+If repurposing, the only changes that need to be made are to the
+insert statements, and
+the data_value variable
+
+Optionally, rename the Table if statements to line up with table being used
+'''
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", action='store', dest='table', required=True, help="Add to table A")
 parser.add_argument("-t", action='store', dest='textfile', required=True, help="Use text file T")
 args = parser.parse_args()
 
-# the name of the file
+# The name of the file
 fileName = args.textfile
 Table    = args.table
 
@@ -23,36 +31,36 @@ line = ff.readline()
 if Table == 'B':
     while line:
         line.split()
-        a,b,c=line.split()
-        insert = ("""INSERT INTO brand(make,brand_worth,public) VALUES(%s,%s,%s)""")
-        data_value = (a,b,c)
+        a,b,c               = line.split()
+        insert              = ("""INSERT INTO brand(make,brand_worth,public) VALUES(%s,%s,%s)""")
+        data_value          = (a,b,c)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # If table flag is BT for Body Type table
 elif Table == 'BT':
     while line:
         line.split()
-        a,b,c,d,e=line.split()
-        insert = ("""INSERT INTO body_type(platform_code,construction,shape,drive,suspension) VALUES(%s,%s,%s,%s,%s)""")
-        data_value = (a,b,c,d,e)
+        a,b,c,d,e           = line.split()
+        insert              = ("""INSERT INTO body_type(platform_code,construction,shape,drive,suspension) VALUES(%s,%s,%s,%s,%s)""")
+        data_value          = (a,b,c,d,e)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # If table flag is C for car table
 elif Table == 'C':
     while line:
         line.split()
-        a,b,c,d,e,f,g=line.split()
-        insert = ("""INSERT INTO car(model,make,price,platform_code,drive,aggregate_review,autonomous) VALUES(%s,%s,%s,%s,%s,%s,%s)""")
-        data_value = (a,b,c,d,e,f,g)
+        a,b,c,d,e,f,g       = line.split()
+        insert              = ("""INSERT INTO car(model,make,price,platform_code,drive,aggregate_review,autonomous) VALUES(%s,%s,%s,%s,%s,%s,%s)""")
+        data_value          = (a,b,c,d,e,f,g)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 
@@ -60,60 +68,60 @@ elif Table == 'C':
 elif Table == 'CO':
     while line:
         line.split()
-        a,b,c,d,e=line.split()
-        insert = ("""INSERT INTO costofownership(model,make,maintenance,depreciation,recalls) VALUES(%s,%s,%s,%s,%s)""")
-        data_value = (a,b,c,d,e)
+        a,b,c,d,e           = line.split()
+        insert              = ("""INSERT INTO costofownership(model,make,maintenance,depreciation,recalls) VALUES(%s,%s,%s,%s,%s)""")
+        data_value          = (a,b,c,d,e)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # If table flag is E for Engine table
 elif Table == 'E':
     while line:
         line.split()
-        a,b,c,d,e,f,g=line.split()
-        insert = ("""INSERT INTO engine(engine_code,cylinder_count,engine_type,hp,torque,fuel_type,induction) VALUES(%s,%s,%s,%s,%s,%s,%s)""")
-        data_value = (a,b,c,d,e,f,g)
+        a,b,c,d,e,f,g       = line.split()
+        insert              = ("""INSERT INTO engine(engine_code,cylinder_count,engine_type,hp,torque,fuel_type,induction) VALUES(%s,%s,%s,%s,%s,%s,%s)""")
+        data_value          = (a,b,c,d,e,f,g)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # If table flag is JP for Joint Project table
 elif Table == 'JP':
     while line:
         line.split()
-        a,b,c,d,e,f,g,h,i=line.split()
-        insert = ("""INSERT INTO jointproject(model,make1,make2,engine_code,platform_code,critic_score,customer_score,average_score,autonomous) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)""")
-        data_value = (a,b,c,d,e,f,g,h,i)
+        a,b,c,d,e,f,g,h,i   =line.split()
+        insert              = ("""INSERT INTO jointproject(model,make1,make2,engine_code,platform_code,critic_score,customer_score,average_score,autonomous) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)""")
+        data_value          = (a,b,c,d,e,f,g,h,i)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # If table flag is R for Review table
 elif Table == 'R':
     while line:
         line.split()
-        a,b,c=line.split()
-        insert = ("""INSERT INTO review(model,critic_score,customer_score) VALUES(%s,%s,%s)""")
-        data_value = (a,b,c)
+        a,b,c               = line.split()
+        insert              = ("""INSERT INTO review(model,critic_score,customer_score) VALUES(%s,%s,%s)""")
+        data_value          = (a,b,c)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # If table flag is T for Tech table
 elif Table == 'T':
     while line:
         line.split()
-        a,b,c,d,e,f=line.split()
-        insert = ("""INSERT INTO tech(model,infotainment,proximity_sensor,auto_braking,adc,autonomous) VALUES(%s,%s,%s,%s,%s,%s)""")
-        data_value = (a,b,c,d,e,f)
+        a,b,c,d,e,f         = line.split()
+        insert              = ("""INSERT INTO tech(model,infotainment,proximity_sensor,auto_braking,adc,autonomous) VALUES(%s,%s,%s,%s,%s,%s)""")
+        data_value          = (a,b,c,d,e,f)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 
@@ -121,12 +129,12 @@ elif Table == 'T':
 elif Table == 'UE':
     while line:
         line.split()
-        a,b=line.split()
-        insert = ("""INSERT INTO usesengine(engine_code,model) VALUES(%s,%s)""")
-        data_value = (a,b)
+        a,b                 = line.split()
+        insert              = ("""INSERT INTO usesengine(engine_code,model) VALUES(%s,%s)""")
+        data_value          = (a,b)
         cur.execute(insert,data_value)
         cnx.commit()
-        line = ff.readline()
+        line                = ff.readline()
     ff.close()
 
 # Fail state for invalid options
